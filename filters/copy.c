4b3f770addd906216c675ebb0e0cb5b30838206b
Kenneth Ye <30275095+kennethye1@users.noreply.github.com>
2020-09-12 16:10:58 -0400

    Update and rename copy.c to filters/copy.c

--- filters/copy.c
+++ filters/copy.c
@@ -8,9 +8,6 @@
  * This function is responsible for doing the following:
  *   1. Read in pixels one at a time (because copy is a pixel-by-pixel transformation).
  *   2. Immediately write out each pixel.
- *
- * Note that this function should allocate space only for a single Pixel;
- * do *not* store more than one Pixel at a time, it isn't necessary here!
  */
 void copy_filter(Bitmap *bmp) {
     for(int i = 0; i < bmp->height * bmp->width; i++){
@@ -28,8 +25,6 @@ void copy_filter(Bitmap *bmp) {
 }
 
 int main() {
-    // Run the filter program with copy_filter to process the pixels.
-    // You shouldn't need to change this implementation.
     run_filter(copy_filter, 1);
     return 0;
 }
