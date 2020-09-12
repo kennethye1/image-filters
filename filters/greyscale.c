96a7a465b1a7a0f9ef055fd98509a0f897b903ab
Kenneth Ye <30275095+kennethye1@users.noreply.github.com>
2020-09-12 16:14:33 -0400

    Update greyscale.c

--- filters/greyscale.c
+++ filters/greyscale.c
@@ -9,9 +9,7 @@
  *   1. Read in pixels one at a time (because copy is a pixel-by-pixel transformation).
  *   2. Change pixels to the average.
      3. Write out pixels right after.
- *
- * Note that this function should allocate space only for a single Pixel;
- * do *not* store more than one Pixel at a time, it isn't necessary here!
+
  */
 void greyscale_filter(Bitmap *bmp) {
     for(int i = 0; i < bmp->height * bmp->width; i++){
@@ -34,8 +32,6 @@ void greyscale_filter(Bitmap *bmp) {
 }
 
 int main() {
-    // Run the filter program with copy_filter to process the pixels.
-    // You shouldn't need to change this implementation.
     run_filter(greyscale_filter, 1);
     return 0;
 }
